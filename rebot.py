1dcddf64af96a27a7b55560d70083b5dadf93bd0
Martin Lensment <mlensment@gmail.com>
2015-03-25 19:49:24 +0200

    Test

--- rebot.py
+++ rebot.py
@@ -1,4 +1,8 @@
 from SimpleCV import *
 
 cam = Camera(0, {"width":1280, "height":1024})
-cam.getImage().save("/home/pi/test.jpg")
+img = cam.getImage()
+blobs = img.findBlobs()
+blobs.draw()
+
+img.save("/home/pi/test.jpg")
