857ebdd248fd5f21f5202ddc0abb6060842a89cc
Martin Lensment <mlensment@gmail.com>
2015-03-25 20:49:00 +0200

    Update

--- display.py
+++ display.py
@@ -1,6 +1,8 @@
 from SimpleCV import*
 cam = Camera(0, {"width":640, "height":480})
+i = 0
 while True:
    img = cam.getImage().grayscale().threshold(45)
    blob = img.findBlobs()
-   img.show()
+   i = i + 1
+   print i
