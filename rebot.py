bdd3d2613c95a8e2f411ef8908c2614731bc4dea
Martin Lensment <mlensment@gmail.com>
2015-03-31 20:37:01 +0300

    Update

--- rebot.py
+++ rebot.py
@@ -4,12 +4,12 @@ cam = Camera(0, {"width":640, "height":640})
 while True:
     img = cam.getImage()
     img = img.rotate(90)
-    bm = BlobMaker()
-    blobs = bm.extractFromBinary(img.invert().binarize(thresh=240).invert(),img)
+    # bm = BlobMaker()
+    # blobs = bm.extractFromBinary(img.invert().binarize(thresh=240).invert(),img)
+    #
+    # if(len(blobs) > 0):
+    #     blobs[0].draw()
+    #     locationStr = "("+str(blobs[0].x)+","+str(blobs[0].y)+")"
+    #     img.dl().text(locationStr,(0,0),color=Color.RED)
 
-    if(len(blobs) > 0):
-        blobs[0].draw()
-        locationStr = "("+str(blobs[0].x)+","+str(blobs[0].y)+")"
-        img.dl().text(locationStr,(0,0),color=Color.RED)
-    
     img.show()
