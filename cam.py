9f96d8e9ddf0c6b332f01dab01e267335d99777b
Martin Lensment <mlensment@gmail.com>
2015-03-31 19:51:06 +0300

    Update

--- cam.py
+++ cam.py
@@ -10,7 +10,7 @@ while True:
     b_img = orig_img.binarize(230)
     m_img = b_img.morphOpen().morphClose()
     inv_img = m_img.invert()
-    blobs = blob_maker.extractFromBinary(inv_img_img, orig_img)
+    blobs = blob_maker.extractFromBinary(inv_img, orig_img)
     if(len(blobs) > 0):
         # centroid = blobs[0].centroid()
         blobs[0].draw()
