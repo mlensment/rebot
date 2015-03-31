02114279d82ed28d9a8209123abc55d7c2935985
Martin Lensment <mlensment@gmail.com>
2015-03-31 20:26:13 +0300

    Update

--- cam.py
+++ cam.py
@@ -7,7 +7,7 @@ while True:
 
     blob_maker = BlobMaker()
     orig_img = img.colorDistance(Color.WHITE)
-    b_img = orig_img.binarize(190) #230
+    b_img = orig_img.binarize(180) #230
     m_img = b_img.morphOpen().morphClose()
     inv_img = m_img.invert()
     blobs = blob_maker.extractFromBinary(inv_img, orig_img)
