dfc575832da9dd8288c102cf680e860ab2cd0c25
Martin Lensment <mlensment@gmail.com>
2015-03-31 19:41:25 +0300

    Update

--- commit.sh
+++ commit.sh
@@ -2,4 +2,4 @@
 
 git add .
 git commit -m "Update"
-git push origin master && ssh pi@192.168.1.139 'cd ~/projects/rebot && git pull origin master'
+git push origin master && ssh pi@192.168.1.171 'cd ~/projects/rebot && git pull origin master'
