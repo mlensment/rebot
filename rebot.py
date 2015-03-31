60458fe0cb957b0e09dca749455743878e463033
Martin Lensment <mlensment@gmail.com>
2015-03-31 20:40:26 +0300

    Update

--- rebot.py
+++ rebot.py
@@ -1,6 +1,7 @@
 from SimpleCV import *
 cam = Camera(0, {"width":640, "height":640})
 
+i = 0
 while True:
     img = cam.getImage()
     img = img.rotate(90)
@@ -11,5 +12,6 @@ while True:
     #     blobs[0].draw()
     #     locationStr = "("+str(blobs[0].x)+","+str(blobs[0].y)+")"
     #     img.dl().text(locationStr,(0,0),color=Color.RED)
-
-    img.show()
+    i += 1
+    print i
+    # img.show()
