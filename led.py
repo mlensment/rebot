b56c2a1fc0f6a95ca03d5f29a1030f64b4f61519
Martin Lensment <mlensment@gmail.com>
2015-04-03 20:45:44 +0300

    Update

--- led.py
+++ led.py
@@ -1,10 +1,11 @@
 import numpy as np
 import cv2
+import cv2.cv as cv
 
 cap = cv2.VideoCapture(0)
 width, height = 320, 320
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-cap.set(cv2.CV_CAP_PROP_FRAME_HEIGHT, height)
+cap.set(cv.CV_CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv.CV_CAP_PROP_FRAME_HEIGHT, height)
 
 # create a window for displaying image and move it to a reasonable spot
 cv2.namedWindow('frame')
