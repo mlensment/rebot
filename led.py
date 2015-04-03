3b68caeb7eaa39a7e73201a8d18f213343721fc3
Martin Lensment <mlensment@gmail.com>
2015-04-03 19:34:24 +0300

    Update

--- led.py
+++ led.py
@@ -1,6 +1,8 @@
+import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
-img = cv2.imread('Puppy_2.jpg', cv2.IMREAD_COLOR)
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+img = cv2.imread('messi5.jpg',0)
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.show()
