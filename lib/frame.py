05c69791173ad2092126b05a88f30271eaca78b6
Martin Lensment <mlensment@gmail.com>
2015-04-04 23:21:00 +0300

    Update

--- lib/frame.py
+++ lib/frame.py
@@ -33,7 +33,7 @@ class Frame:
 
         # get rid of too bright pixels
         # ret, mask = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
-        mask = cv2.inRange(img, 200, 255)
+        mask = cv2.inRange(img, np.array([200]), np.array([255]))
         cv2.imshow(config.WINDOW_NAME, mask)
 
         inRange(img, RangeLow,RangeHigh,Mask);
