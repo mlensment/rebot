23ecbc9c08817c4968eb3834dd54d18f9f1ff6fe
Martin Lensment <mlensment@gmail.com>
2015-04-04 16:43:59 +0300

    Update

--- rebot.py
+++ rebot.py
@@ -19,7 +19,7 @@ class Rebot:
         while(1):
             frame = self.camera.read_frame()
             frame.process()
-            frame.show()
+            frame.show_original()
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
