614802c415dfe128cde881f711773b1486dc8b25
Martin Lensment <mlensment@gmail.com>
2015-04-05 00:15:05 +0300

    Update

--- lib/frame.py
+++ lib/frame.py
@@ -61,7 +61,7 @@ class Frame:
 
         edges = cv2.Canny(img, 50, 130)
 
-        circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 20,
+        circles = cv2.HoughCircles(edges, cv.HOUGH_GRADIENT, 1, 20,
                             param1=50,param2=30,minRadius=0,maxRadius=0)
 
         circles = np.uint16(np.around(circles))
