3cf50d0d5d7b8ca8937eac337b754dbc54412fbb
Martin Lensment <mlensment@gmail.com>
2015-04-05 00:14:09 +0300

    Update

--- lib/frame.py
+++ lib/frame.py
@@ -60,8 +60,19 @@ class Frame:
 
 
         edges = cv2.Canny(img, 50, 130)
-        cv2.imshow(config.WINDOW_NAME, edges)
 
+        circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 20,
+                            param1=50,param2=30,minRadius=0,maxRadius=0)
+
+        circles = np.uint16(np.around(circles))
+
+        for i in circles[0,:]:
+            # draw the outer circle
+            cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
+            # draw the center of the circle
+            cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
+
+        cv2.imshow(config.WINDOW_NAME, img)
         # ret, thresh = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
         # contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
