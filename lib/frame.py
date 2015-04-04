d9183acaa60ed3bd5c411df53f9d401001a959a9
Martin Lensment <mlensment@gmail.com>
2015-04-05 01:56:44 +0300

    Update

--- lib/frame.py
+++ lib/frame.py
@@ -87,10 +87,15 @@ class Frame:
                 contour = i
                 largest_area = area
 
-        cv2.drawContours(self.processed, contour, -1, (0,255,0), 3)
+        (x,y),radius = cv2.minEnclosingCircle(contour)
+        center = (int(x),int(y))
+        radius = int(radius)
+        img = cv2.circle(img,center,radius,(0,255,0),2)
+        #
+        # cv2.drawContours(self.processed, contour, -1, (0,255,0), 3)
 
 
-        cv2.imshow('3', self.processed)
+        cv2.imshow('3', img)
 
         # cv2.imshow(config.WINDOW_NAME, img)
         # edges = cv2.Canny(img, 0, 200)
