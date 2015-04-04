9337062b4744c4465a763e1f5bf2009a1226f3f7
Martin Lensment <mlensment@gmail.com>
2015-04-04 12:57:22 +0300

    Update

--- argtest.py
+++ argtest.py
@@ -1,11 +1,11 @@
 import argparse
+__author__ = 'nixCraft'
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                   help='an integer for the accumulator')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
-                   const=sum, default=max,
-                   help='sum the integers (default: find the max)')
-
+parser = argparse.ArgumentParser(description='This is a demo script by nixCraft.')
+parser.add_argument('-i','--input', help='Input file name',required=True)
+parser.add_argument('-o','--output',help='Output file name', required=True)
 args = parser.parse_args()
-print args.accumulate(args.integers)
+
+## show values ##
+print ("Input file: %s" % args.input )
+print ("Output file: %s" % args.output )
