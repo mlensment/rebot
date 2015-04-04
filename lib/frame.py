15206a55524402bee0d79523475c36d366f7ae4d
Martin Lensment <mlensment@gmail.com>
2015-04-04 18:21:22 +0300

    Update

--- lib/frame.py
+++ lib/frame.py
@@ -28,11 +28,11 @@ class Frame:
     def find_glints(self):
         # convert to grayscale
         gray = cv2.cvtColor(self.original, cv2.COLOR_BGR2GRAY)
+        cv2.imshow(config.WINDOW_NAME, gray)
 
         # threshold the image
         ret, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
 
-        cv2.imshow(config.WINDOW_NAME, thresh)
         contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
         self.processed = cv2.drawContours(self.original, contours, -1, (0,255,0), 3)
 
