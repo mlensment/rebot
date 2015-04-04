e64201a3dcc9623e35a9dc8e298bea538a4dfab2
Martin Lensment <mlensment@gmail.com>
2015-04-04 16:15:22 +0300

    Update

--- lib/camera.py
+++ lib/camera.py
@@ -32,7 +32,9 @@ class Camera:
 
     # Reads raw frame and creates a Frame object
     def read_frame(self):
-        return self.cached_frame if self.cached_frame else return frame.Frame(self.read())
+        if self.cached_frame:
+            return self.cached_frame
+        return frame.Frame(self.read())
 
     def snapshot(self, filename = 'shot.jpg'):
         cv2.imwrite(filename, self.read())
