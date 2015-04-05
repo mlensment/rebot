f0bb81120d122c186f7f2d39d5a70805a23e5c05
Martin Lensment <mlensment@gmail.com>
2015-04-05 15:42:34 +0300

    Update

--- lib/frame.py
+++ lib/frame.py
@@ -34,29 +34,29 @@ class Frame:
 
         # get rid of too bright pixels
         # create gray background
-        blank_image = np.zeros(img.shape, np.uint8)
-        blank_image[:, :] = 100
+        # blank_image = np.zeros(img.shape, np.uint8)
+        # blank_image[:, :] = 100
+        #
+        # # create mask for too bright areas
+        # ret, mask = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
+        # mask_inv = cv2.bitwise_not(mask)
+        #
+        # # paint all normal areas black on background
+        # img1_bg = cv2.bitwise_and(img, img, mask = mask_inv)
+        #
+        # # paint all bright areas black on foreground
+        # img2_fg = cv2.bitwise_and(blank_image, blank_image, mask = mask)
+        #
+        # # join two images
+        # img = cv2.add(img1_bg, img2_fg)
+        #
+        # cv2.imshow(config.WINDOW_NAME, img1_bg)
+        #
+        # # erode remaining white areas
+        # kernel = np.ones((10,10), np.uint8)
+        # img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 
-        # create mask for too bright areas
-        ret, mask = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
-        mask_inv = cv2.bitwise_not(mask)
-
-        # paint all normal areas black on background
-        img1_bg = cv2.bitwise_and(img, img, mask = mask_inv)
-
-        # paint all bright areas black on foreground
-        img2_fg = cv2.bitwise_and(blank_image, blank_image, mask = mask)
-
-        # join two images
-        img = cv2.add(img1_bg, img2_fg)
-
-        cv2.imshow(config.WINDOW_NAME, img1_bg)
-
-        # erode remaining white areas
-        kernel = np.ones((10,10), np.uint8)
-        img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-
-        cv2.imshow('4', img)
+        # cv2.imshow('4', img)
 
         # find edges
         # values 50, 130 work well
