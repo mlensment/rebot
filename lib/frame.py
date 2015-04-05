3dc4a3177002b8ca8213eec23b23ef526f240b22
Martin Lensment <mlensment@gmail.com>
2015-04-05 13:38:10 +0300

    Update

--- lib/frame.py
+++ lib/frame.py
@@ -32,60 +32,65 @@ class Frame:
         self.processed = self.original
         img = cv2.cvtColor(self.processed, cv2.COLOR_BGR2GRAY)
 
+        # try to just threshold for fun
+        ret, mask = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+        cv2.imshow('3', mask)
+
+
         # get rid of too bright pixels
         # create gray background
-        blank_image = np.zeros(img.shape, np.uint8)
-        blank_image[:, :] = 100
-
-        # create mask for too bright areas
-        ret, mask = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
-        mask_inv = cv2.bitwise_not(mask)
-
-        # paint all normal areas black on background
-        img1_bg = cv2.bitwise_and(img, img, mask = mask_inv)
-
-        # paint all bright areas black on foreground
-        img2_fg = cv2.bitwise_and(blank_image, blank_image, mask = mask)
-
-        # join two images
-        img = cv2.add(img1_bg, img2_fg)
-
-        cv2.imshow(config.WINDOW_NAME, img1_bg)
-
-        # erode remaining white areas
-        kernel = np.ones((10,10), np.uint8)
-        img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-
-        cv2.imshow('4', img)
-
-        # find edges
-        edges = cv2.Canny(img, 50, 130)
-        cv2.imshow('2', edges)
-
-        # find contours
-        contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        # Find largest contour
-        largest_area = 0
-        contour = None
-        for i in contours:
-            area = cv2.contourArea(i)
-            if area >= largest_area and area >= 50:
-                contour = i
-                largest_area = area
-
-        if contour is not None and contour.any():
-            (x,y),radius = cv2.minEnclosingCircle(contour)
-            center = (int(x),int(y))
-            radius = int(radius)
-            img = cv2.circle(self.original,center,radius,(0,255,0),2)
-            #
-            # cv2.drawContours(self.processed, contour, -1, (0,255,0), 3)
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(self.original, 'Largest contour area: ' + str(largest_area), (10,10), font, 0.3, (255,255,255), 2)
-            cv2.putText(self.original, 'Center of the contour: (' + str(x) + ', ' + str(y) + ')', (10,30), font, 0.3, (255,255,255), 2)
-
-        cv2.imshow('3', self.original)
+        # blank_image = np.zeros(img.shape, np.uint8)
+        # blank_image[:, :] = 100
+        #
+        # # create mask for too bright areas
+        # ret, mask = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
+        # mask_inv = cv2.bitwise_not(mask)
+        #
+        # # paint all normal areas black on background
+        # img1_bg = cv2.bitwise_and(img, img, mask = mask_inv)
+        #
+        # # paint all bright areas black on foreground
+        # img2_fg = cv2.bitwise_and(blank_image, blank_image, mask = mask)
+        #
+        # # join two images
+        # img = cv2.add(img1_bg, img2_fg)
+        #
+        # cv2.imshow(config.WINDOW_NAME, img1_bg)
+        #
+        # # erode remaining white areas
+        # kernel = np.ones((10,10), np.uint8)
+        # img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+        #
+        # cv2.imshow('4', img)
+        #
+        # # find edges
+        # edges = cv2.Canny(img, 50, 130)
+        # cv2.imshow('2', edges)
+        #
+        # # find contours
+        # contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        #
+        # # Find largest contour
+        # largest_area = 0
+        # contour = None
+        # for i in contours:
+        #     area = cv2.contourArea(i)
+        #     if area >= largest_area and area >= 50:
+        #         contour = i
+        #         largest_area = area
+        #
+        # if contour is not None and contour.any():
+        #     (x,y),radius = cv2.minEnclosingCircle(contour)
+        #     center = (int(x),int(y))
+        #     radius = int(radius)
+        #     img = cv2.circle(self.original,center,radius,(0,255,0),2)
+        #     #
+        #     # cv2.drawContours(self.processed, contour, -1, (0,255,0), 3)
+        #     font = cv2.FONT_HERSHEY_SIMPLEX
+        #     cv2.putText(self.original, 'Largest contour area: ' + str(largest_area), (10,10), font, 0.3, (255,255,255), 2)
+        #     cv2.putText(self.original, 'Center of the contour: (' + str(x) + ', ' + str(y) + ')', (10,30), font, 0.3, (255,255,255), 2)
+        #
+        # cv2.imshow('3', self.original)
 
         # cv2.imshow(config.WINDOW_NAME, img)
         # edges = cv2.Canny(img, 0, 200)
