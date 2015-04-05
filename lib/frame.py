38823162446db2bdd80923784f3adf842e9cfdf7
Martin Lensment <mlensment@gmail.com>
2015-04-05 15:52:07 +0300

    Update

--- lib/frame.py
+++ lib/frame.py
@@ -54,8 +54,7 @@ class Frame:
         #
         # erode remaining white areas
         kernel = np.ones((10,10), np.uint8)
-        img = cv2.erode(img, kernel, iterations = 1)
-        # img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+        img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 
         cv2.imshow('4', img)
 
