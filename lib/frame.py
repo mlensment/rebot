3e1b00752af1a53cdb795ebfcb7125ee10f3bf54
Martin Lensment <mlensment@gmail.com>
2015-04-05 17:19:17 +0300

    Update

--- lib/frame.py
+++ lib/frame.py
@@ -72,11 +72,11 @@ class Frame:
         #     print e
         # self.printed = True
 
-        minLineLength = 100
-        maxLineGap = 10
-        lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
-        for x1,y1,x2,y2 in lines[0]:
-            cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+        # minLineLength = 100
+        # maxLineGap = 10
+        # lines = cv2.HoughLinesP(contours,1,np.pi/180,100,minLineLength,maxLineGap)
+        # for x1,y1,x2,y2 in lines[0]:
+        #     cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
 
         if contour is not None and contour.any():
             (x,y),radius = cv2.minEnclosingCircle(contour)
