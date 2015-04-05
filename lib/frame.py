81a3c58fb6dde36d386efb2f9bfa4665a358f8d4
Martin Lensment <mlensment@gmail.com>
2015-04-05 14:13:28 +0300

    Update

--- lib/frame.py
+++ lib/frame.py
@@ -38,7 +38,7 @@ class Frame:
         blank_image[:, :] = 100
 
         # create mask for too bright areas
-        ret, mask = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
+        ret, mask = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
         mask_inv = cv2.bitwise_not(mask)
 
         # paint all normal areas black on background
@@ -60,7 +60,7 @@ class Frame:
 
         # find edges
         # values 50, 130 work well
-        edges = cv2.Canny(img, 40, 120)
+        edges = cv2.Canny(img, 40, 130)
 
         kernel = np.ones((3,3), np.uint8)
         edges = cv2.dilate(edges, kernel, iterations = 1)
