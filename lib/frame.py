ecd2f57096c6c0ed73e43e6983f7037724bf770f
Martin Lensment <mlensment@gmail.com>
2015-04-05 13:52:34 +0300

    Update

--- lib/frame.py
+++ lib/frame.py
@@ -62,7 +62,7 @@ class Frame:
         # values 50, 130 work well
         edges = cv2.Canny(img, 40, 130)
 
-        kernel = np.ones((2,2), np.uint8)
+        kernel = np.ones((3,3), np.uint8)
         edges = cv2.dilate(edges, kernel, iterations = 1)
         cv2.imshow('2', edges)
 
