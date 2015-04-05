c50fdda139f9eb73f47dac9a6e673d94fdd34650
Martin Lensment <mlensment@gmail.com>
2015-04-05 16:41:52 +0300

    Update

--- lib/frame.py
+++ lib/frame.py
@@ -74,7 +74,7 @@ class Frame:
 
         minLineLength = 100
         maxLineGap = 10
-        lines = cv2.HoughLinesP(contours,1,np.pi/180,100,minLineLength,maxLineGap)
+        lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
         for x1,y1,x2,y2 in lines[0]:
             cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
 
