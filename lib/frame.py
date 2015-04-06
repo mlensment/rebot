38f60c8491570c7dd0c8f6f41267e1d99acc0279
Martin Lensment <mlensment@gmail.com>
2015-04-06 19:21:06 +0300

    Update

--- lib/frame.py
+++ lib/frame.py
@@ -46,9 +46,9 @@ class Frame:
         edges = cv2.Canny(img, 40, 130)
         # kernel = np.ones((2,2), np.uint8)
         # edges = cv2.dilate(edges, kernel, iterations = 1)
-        cv2.imshow('4', edges)
+        cv2.imshow('2', edges)
 
-        edges = cv2.Canny(img, 10, 130)
+        edges = cv2.Canny(img, 10, 200)
         cv2.imshow('4', edges)
 
         # find contours
