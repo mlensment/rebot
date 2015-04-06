473ca03169e3d7e45928284dae2ecb8683c014b1
Martin Lensment <mlensment@gmail.com>
2015-04-06 20:22:21 +0300

    Update

--- lib/frame.py
+++ lib/frame.py
@@ -96,7 +96,6 @@ class Frame:
             cv2.putText(self.processed, 'Largest contour area: ' + str(round(largest_area, 2)), (10,30), font, 0.5, (255,255,255), 2)
             cv2.putText(self.processed, 'Center of the contour: (' + str(round(x, 2)) + ', ' + str(round(y, 2)) + ')', (10,60), font, 0.5, (255,255,255), 2)
             cv2.putText(self.processed, 'Radius of the contour: ' + str(round(radius, 2)), (10,90), font, 0.5, (255,255,255), 2)
-            cv2.putText(self.processed, 'x: ' + str(round((x - radius), 2)), (10,120), font, 0.5, (255,255,255), 2)
 
         cv2.imshow('3', self.processed)
 
