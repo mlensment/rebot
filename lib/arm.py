bebb0cc2ec9be9a1228fb62ba217f14fd126f578
Martin Lensment <mlensment@gmail.com>
2015-04-12 00:04:29 +0300

    Update

--- lib/arm.py
+++ lib/arm.py
@@ -1,15 +1,12 @@
+import os
+
 class Arm:
     def __init__(self):
         pass
 
     def move_to(self, deg):
-        try:
-            f = open("/dev/servoblaster", 'w')
-    		f.write('2=' + deg)
-            f.write('5=' + deg)
-    		f.close()
-        except:
-		    print("Error writing to servoblaster. Is the driver loaded?")
+        os.system("echo 2=" + deg + " > /dev/servoblaster")
+        os.system("echo 5=" + deg + " > /dev/servoblaster")
 
 a = Rebot()
 a.move_to(120)
