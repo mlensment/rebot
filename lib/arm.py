513aa3b55a1399e246672b6da23aa75d86f4490b
Martin Lensment <mlensment@gmail.com>
2015-04-12 02:54:05 +0300

    Update

--- lib/arm.py
+++ lib/arm.py
@@ -8,7 +8,7 @@ class Arm:
     def __init__(self):
         # load driver, redirect stdout to /dev/null
         # although servoblaster seems to be writing errors to stdout too, instead of stderr...
-        os.system("sudo killall servod && sudo ./../bin/servod 1> /dev/null")
+        # os.system("sudo killall servod && sudo ./../bin/servod 1> /dev/null")
 
         self.spoon_position = 0
         self.spoon_next_pos = 0
@@ -19,21 +19,20 @@ class Arm:
         start_position = self.spoon_position
         end_position = deg
         millis = int(round(time.time() * 1000))
-
         while(self.spoon_position != deg):
             elapsed_time = int(round(time.time() * 1000)) - millis
 
             self.spoon_position = self.ease_in_out_quad(self.spoon_position, elapsed_time, start_position, end_position, 2000)
-            print elapsed_time
-            # self.move_to(5, self.spoon_position)
+            self.spoon_position = math.ceil(self.spoon_position)
+            self.move_to(5, self.spoon_position)
             # self.spoon_position += 1
 
     def ease_in_out_quad(self, x, t, b, c, d):
-        ti = d / 2
-        ti /= ti
-        if (ti < 1):
-             return c/2*ti*ti + b
-        return -c/2 * ((--t)*(t-2) - 1) + b
+        t = t / (d / 2.0)
+        if (t < 1):
+             return c/2*t*t + b
+        ti = t - 1
+        return -c/2 * ((ti)*(t-3) - 1) + b
 
     def move_to(self, servo, deg):
         pwm = deg * 0.94 # 1 degree = 0.094ms high pulse time
