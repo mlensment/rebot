7ef5fa9b93f06a54603a13fc527f91a69aafe29b
Martin Lensment <mlensment@gmail.com>
2015-04-12 00:44:36 +0300

    Update

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
 WINDOW_NAME = "frame"
+
+BOTTOM_SERVO = 2
+TOP_SERVO = 5
+# 0 on P1-7           GPIO-4
+# 1 on P1-11          GPIO-17
+# 2 on P1-12          GPIO-18
+# 3 on P1-13          GPIO-21
+# 4 on P1-15          GPIO-22
+# 5 on P1-16          GPIO-23
+# 6 on P1-18          GPIO-24
+# 7 on P1-22          GPIO-25
--- lib/arm.py
+++ lib/arm.py
@@ -2,12 +2,18 @@ import os
 
 class Arm:
     def __init__(self):
+        # load driver, redirect stdout to /dev/null
+        # although servoblaster seems to be writing errors to stdout too, instead of stderr...
         os.system("sudo killall servod && sudo ./../bin/servod 1> /dev/null")
 
     def move_to(self, deg):
+        pwm = deg * 1.11 # 1 degree = 0.011ms high pulse time
+        pwm += 50  # minimum high pulse time is 0.5 milliseconds
+
+
         if os.path.exists('/dev/servoblaster'):
-            os.system("echo 2=" + str(deg) + " > /dev/servoblaster")
-            os.system("echo 5=" + str(deg) + " > /dev/servoblaster")
+            os.system("echo 2=" + str(ceil(deg)) + " > /dev/servoblaster")
+            os.system("echo 5=" + str(ceil(deg)) + " > /dev/servoblaster")
         else:
             raise 'ERROR: Servo driver was not found. Is servoblaster loaded?'
 
