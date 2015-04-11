5b5a8f931d2dcc25bdda38205a1b76f6c0b5f19b
Martin Lensment <mlensment@gmail.com>
2015-04-12 00:05:08 +0300

    Update

--- lib/arm.py
+++ lib/arm.py
@@ -5,8 +5,8 @@ class Arm:
         pass
 
     def move_to(self, deg):
-        os.system("echo 2=" + deg + " > /dev/servoblaster")
-        os.system("echo 5=" + deg + " > /dev/servoblaster")
+        os.system("echo 2=" + str(deg) + " > /dev/servoblaster")
+        os.system("echo 5=" + str(deg) + " > /dev/servoblaster")
 
 a = Arm()
 a.move_to(120)
