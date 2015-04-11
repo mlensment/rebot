62b942f89c036b3a801c0ffcf996465d2d154a57
Martin Lensment <mlensment@gmail.com>
2015-04-12 00:11:58 +0300

    Update

--- lib/arm.py
+++ lib/arm.py
@@ -2,11 +2,14 @@ import os
 
 class Arm:
     def __init__(self):
-        pass
+        os.system("sudo ./../bin/servod")
 
     def move_to(self, deg):
-        os.system("echo 2=" + str(deg) + " > /dev/servoblaster")
-        os.system("echo 5=" + str(deg) + " > /dev/servoblaster")
+        if os.path.isfile('/dev/servoblaster'):
+            os.system("echo 2=" + str(deg) + " > /dev/servoblaster")
+            os.system("echo 5=" + str(deg) + " > /dev/servoblaster")
+        else:
+            raise 'ERROR: Servo driver was not found. Is servoblaster loaded?'
 
 a = Arm()
 a.move_to(120)
