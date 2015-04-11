134e32cc330a0c99ff74dfc31b8924ea4a645920
Martin Lensment <mlensment@gmail.com>
2015-04-12 00:53:13 +0300

    Update

--- lib/arm.py
+++ lib/arm.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import math
 
@@ -11,12 +12,15 @@ class Arm:
         pwm = deg * 1.11 # 1 degree = 0.011ms high pulse time
         pwm += 50  # minimum high pulse time is 0.5 milliseconds
 
-
         if os.path.exists('/dev/servoblaster'):
             os.system("echo 2=" + str(math.ceil(pwm)) + " > /dev/servoblaster")
             os.system("echo 5=" + str(math.ceil(pwm)) + " > /dev/servoblaster")
         else:
             raise 'ERROR: Servo driver was not found. Is servoblaster loaded?'
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--deg', required=True, type=int)
+args = parser.parse_args()
+
 a = Arm()
-a.move_to(0)
+a.move_to(args.deg)
