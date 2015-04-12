76cf449c6857b94b51335d0beab266e112feb1fb
Martin Lensment <mlensment@gmail.com>
2015-04-12 19:57:14 +0300

    Update

---
+++ lib/config.py
@@ -0,0 +1,12 @@
+WINDOW_NAME = "frame"
+
+SPOON_SERVO = 5
+LEG_SERVO = 2
+# 0 on P1-7           GPIO-4
+# 1 on P1-11          GPIO-17
+# 2 on P1-12          GPIO-18
+# 3 on P1-13          GPIO-21
+# 4 on P1-15          GPIO-22
+# 5 on P1-16          GPIO-23
+# 6 on P1-18          GPIO-24
+# 7 on P1-22          GPIO-25
