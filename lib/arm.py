4d0c5a0ee7e0b005a5c519138afa0a062469fc33
Martin Lensment <mlensment@gmail.com>
2015-04-14 00:00:39 +0300

    Update

--- lib/arm.py
+++ lib/arm.py
@@ -2,120 +2,35 @@ import argparse
 import os
 import math
 import time
-import camera, frame
-import cv2
+import servo
 
 class Arm:
     def __init__(self):
-        # load driver, redirect stdout to /dev/null
-        # although servoblaster seems to be writing errors to stdout too, instead of stderr...
-        # os.system("sudo killall servod && sudo ./../bin/servod 1> /dev/null")
+        self.init_servos()
 
-        # sudo ./servod --step-seize 2
-        # range 250 - 1250
+    def init_servos(self):
+        self.spoon_servo = servo.Servo(2)
+        self.spoon_servo.start()
 
-        # sudo ./servod --step-seize 2
-        # range 50 - 250
-
-        self.spoon_position = 250
-        # temp
-        self.camera = camera.Camera()
-        cv2.namedWindow('frame')
-        cv2.moveWindow('frame', 100, 100)
-
-        cv2.namedWindow('2')
-        cv2.moveWindow('2', 500, 100)
-
-        cv2.namedWindow('3')
-        cv2.moveWindow('3', 1000, 100)
-
-        cv2.namedWindow('4')
-        cv2.moveWindow('4', 100, 500)
-
-    def ease_spoon_to(self, deg):
-        start_position = self.spoon_position
-        end_position = deg
-        millis = int(round(time.time() * 1000))
-
-        direction = 'asc'
-        if self.spoon_position > deg:
-            direction = 'desc'
-
+    def run(self):
+        i = 0
         while(1):
-            frame = self.camera.read_frame()
-            frame.find_glints()
+            pass
+            # is_alive()
+            self.ease_spoon(180)
+            self.ease_spoon(0)
 
-            elapsed_time = int(round(time.time() * 1000)) - millis
-
-            self.spoon_position = self.ease_in_out_sine(elapsed_time, start_position, end_position, 15000)
-
-            if math.ceil(self.spoon_position) >= deg and direction == 'asc':
-                break
-
-            if math.floor(self.spoon_position) <= deg and direction == 'desc':
-                break
-
-            if self.spoon_position > 1250:
-                self.spoon_position = 1250
-            elif self.spoon_position < 250:
-                self.spoon_position = 250
-
-            # print str(self.spoon_position) + " - " + str(elapsed_time) + " - " + str(start_position) + " - " + str(end_position)
-            self.move_to(2, self.spoon_position)
-
-            if cv2.waitKey(1) & 0xFF == ord('/'):
-                break
-
-    def ease_in_out_sine(self, elapsed_time, begin, end, timeframe):
-        b = begin
-        c = math.fabs(end - begin)
-        begin = 0
-
-        pos = -c / 2 * (math.cos(math.pi * elapsed_time / timeframe) - 1)
-
-        if b > end:
-            return math.fabs(pos - b)
-        else:
-            return pos + b
+            # self.spoon_servo.terminate()
+            # time.sleep(5)
+            # self.ease_spoon(180)
+            # i += 1
 
 
-        # c = math.abs(c - b)
-        # b = 0
-        #
-        # return -c / 2 * (math.cos(math.pi * t / d) - 1) + b
+    def ease_spoon(self, deg):
+        self.spoon_servo.angle_to.value = float(deg)
+        print 'setting angle'
+        print deg
+        # self.spoon_servo.start()
 
-        # t = t / (d / 2.0)
-        # if (t < 1):
-        #      return c/2*t*t + b
-        # ti = t - 1
-        # return -c/2 * ((ti)*(t-3) - 1) + b
-
-    def move_to(self, servo, deg):
-        pwm = deg
-        # pwm = deg * 0.94 # 1 degree = 0.094ms high pulse time
-        # pwm += 50  # minimum high pulse time is 0.5 milliseconds
-
-        if os.path.exists('/dev/servoblaster'):
-            # os.system("echo 2=" + str(math.ceil(pwm)) + " > /dev/servoblaster")
-            os.system("echo " + str(servo) + "=" + str(math.ceil(pwm)) + " > /dev/servoblaster")
-        else:
-            raise 'ERROR: Servo driver was not found. Is servoblaster loaded?'
-
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--deg', required=True, type=int)
-# args = parser.parse_args()
-#
 a = Arm()
-# a.move_to(5, 0)
-print 'easing to 60'
-a.ease_spoon_to(250)
-print 'easing to 250'
-a.ease_spoon_to(1250)
-print 'easing to 60'
-a.ease_spoon_to(250)
-print 'easing to 250 last'
-a.ease_spoon_to(1250)
-a.ease_spoon_to(250)
-
-# a.camera.close()
-# cv2.destroyAllWindows()
+a.run()
--- lib/servo.py
+++ lib/servo.py
@@ -1,15 +1,15 @@
-from multiprocessing import Process, Queue, Value
+from multiprocessing import Process, Value
 import time
 import math
 import os
 import config
 
 class Servo(Process):
-    def __init__(self, servo, angle):
+    def __init__(self, servo):
         Process.__init__(self)
         self.servo = servo
 
-        self.angle = angle
+        self.angle = Value('f', 0.0)
         self.angle_to = Value('f', 0.0)
 
         self.daemon = True
@@ -86,88 +86,3 @@ class Servo(Process):
     @staticmethod
     def time_in_millis():
         return int(round(time.time() * 1000))
-
-class App:
-    def __init__(self):
-        self.spoon_angle = Value('f', 0.0) # shared memory between sub and main process
-        self.init_servos()
-
-    def init_servos(self):
-        self.spoon_servo = Servo(2, self.spoon_angle)
-        self.spoon_servo.start()
-
-    def run(self):
-        i = 0
-        while(1):
-            pass
-            # is_alive()
-            self.ease_spoon(180)
-            self.ease_spoon(0)
-
-            # self.spoon_servo.terminate()
-            # time.sleep(5)
-            # self.ease_spoon(180)
-            # i += 1
-
-
-    def ease_spoon(self, deg):
-        self.spoon_servo.angle_to.value = float(deg)
-        print 'setting angle'
-        print deg
-        # self.spoon_servo.start()
-
-a = App()
-a.run()
-# a.ease_to(10)
-
-# if __name__ == '__main__':
-# num = Value('i', 0)
-# spoon_servo = Servo(2, num)
-# spoon_servo.daemon = True
-# spoon_servo.start()
-#
-# i = 0
-# while(1):
-#     # print num.value
-#     print spoon_servo.angle.value
-#     i += 1
-#     if i > 1000:
-#         break
-#
-
-
-# if __name__ == '__main__':
-#     num = Value('d', 0.0)
-#
-#     p = Process(target=f, args=(num))
-#     p.start()
-#     p.join()
-#
-#     time.sleep(3)
-#     print num.value
-
-# from multiprocessing import Process, Queue
-# import time
-#
-# class Worker(Process):
-#     def __init__(self, queue):
-#         Process.__init__(self)
-#         self.queue = queue
-#         self.running = True
-#
-#     def run(self):
-#         print 'entered run'
-#         while self.running:
-#             print 'thread time:', time.time()
-#             time.sleep(.6)
-#         print 'thread end'
-#         return 0
-# #
-# if __name__ == '__main__':
-#     queue = Queue()
-#     p = Worker(queue)
-#     p.daemon = True
-#     p.start()
-#     time.sleep(3)
-#     p.running = False
-#     print 'Main end'
