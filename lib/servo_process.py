d13d6ad7cb488f87cf5650f6292ba2ae3bd026c0
Martin Lensment <mlensment@gmail.com>
2015-04-21 20:10:41 +0300

    Fix min values

--- lib/servo_process.py
+++ lib/servo_process.py
@@ -9,7 +9,7 @@ class ServoProcess(Process):
     def __init__(self):
         Process.__init__(self)
         self.spoon = servo.Servo(2, 0.0)
-        self.leg = servo.Servo(5, 20.0)
+        self.leg = servo.Servo(5, 0.0)
 
     def run(self):
         print 'entered run'
