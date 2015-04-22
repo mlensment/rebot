f680423d86618da204beebff2bc09b43a86e19f6
Martin Lensment <mlensment@gmail.com>
2015-04-22 22:40:54 +0300

    Update

--- lib/arm.py
+++ lib/arm.py
@@ -94,7 +94,7 @@ class Arm:
 
         print 'EXTENDING'
         self.leg_status = 'extending'
-        self.sp.leg.rotate(90, 5000)
+        self.sp.leg.rotate(80, 5000)
         self.sp.spoon.sleep(5000)
 
     def shut_down(self):
