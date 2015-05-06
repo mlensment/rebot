9ace58b33df83ef4ebb2f4967a2b47eb08547e8a
Martin Lensment <mlensment@gmail.com>
2015-05-06 23:54:07 +0300

    Update

---
+++ launch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export LD_PRELOAD=/usr/lib/uv4l/uv4lext/armv6l/libuv4lext.so
+python rebot.py
--- led.py
+++ led.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(25, GPIO.OUT)
-GPIO.output(25, True) #
+GPIO.setup(12, GPIO.OUT)
+GPIO.output(12, True) #
 
 time.sleep(5)
-GPIO.output(25, False) #
+GPIO.output(12, False) #
