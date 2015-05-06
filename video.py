4a457187d77dd04c1117ced9d28f9534326abf1f
Martin Lensment <mlensment@gmail.com>
2015-05-06 16:54:15 +0300

    Update

--- video.py
+++ video.py
@@ -3,9 +3,9 @@ import cv2
 import cv2.cv as cv
 
 cap = cv2.VideoCapture(0)
-width, height = 320, 320
-cap.set(cv.CV_CAP_PROP_FRAME_WIDTH, width)
-cap.set(cv.CV_CAP_PROP_FRAME_HEIGHT, height)
+# width, height = 320, 320
+# cap.set(cv.CV_CAP_PROP_FRAME_WIDTH, width)
+# cap.set(cv.CV_CAP_PROP_FRAME_HEIGHT, height)
 
 while(True):
     ret, frame = cap.read()
