0bc12385921fc2586fc34057503d28a52797c4e3
Martin Lensment <mlensment@gmail.com>
2015-05-08 00:53:33 +0300

    Update

--- lib/frame.py
+++ lib/frame.py
@@ -12,7 +12,7 @@ class Frame:
         self.printed = False
 
     def find_eye(self):
-        cv2.imshow('4', self.original)
+        cv2.imshow('1', self.original)
         self.processed = self.original.copy()
 
         # convert to grayscale
@@ -22,16 +22,16 @@ class Frame:
         kernel = np.ones((10,10), np.uint8)
         img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 
-        cv2.imshow('1', img)
+        cv2.imshow('2', img)
 
         # find edges
         # values 50, 130 work well
         # values 40, 130 work well
         edges = cv2.Canny(img, 40, 130)
-        cv2.imshow('2', edges)
+        cv2.imshow('3', edges)
         # find contours
         contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+        cv2.imshow('4', contours)
         # find largest contour
         largest_area = 0
         contour = None
