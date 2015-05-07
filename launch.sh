30c01cca2fcc63013be15cbe9b45433695ff43e5
Martin Lensment <mlensment@gmail.com>
2015-05-08 00:28:09 +0300

    Update

--- launch.sh
+++ launch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 export LD_PRELOAD=/usr/lib/uv4l/uv4lext/armv6l/libuv4lext.so
 if [ -z "$1" ]; then
-  python rebot.py --frame $1
-else
   python rebot.py
+else
+  python rebot.py --frame $1
 fi
