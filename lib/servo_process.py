40eebc14125e3ebd2f9f0bc976da1b60262408ec
Martin Lensment <mlensment@gmail.com>
2015-05-10 03:04:32 +0300

    Update

--- lib/servo_process.py
+++ lib/servo_process.py
@@ -18,21 +18,24 @@ class ServoProcess(Process):
         self.initialized = Value('b', False)
 
     def run(self):
-        signal.signal(signal.SIGINT, signal.SIG_IGN)
-        print '----> Initiating servo calibration sequence...'
+        # signal.signal(signal.SIGINT, signal.SIG_IGN)
+        try:
+            print '----> Initiating servo calibration sequence...'
 
-        i = config.SERVO_MAX_WIDTH - config.SERVO_MIN_WIDTH
-        while(i > 0):
-            self.spoon.decrease_pwm(10)
-            self.leg.decrease_pwm(10)
-            i -= 10
+            i = config.SERVO_MAX_WIDTH - config.SERVO_MIN_WIDTH
+            while(i > 0):
+                self.spoon.decrease_pwm(10)
+                self.leg.decrease_pwm(10)
+                i -= 10
 
-        print '----> Servo calibration complete'
-        self.initialized.value = True
+            print '----> Servo calibration complete'
+            self.initialized.value = True
 
-        while(1):
-            self.spoon.update()
-            self.spoon.alter_pwm()
+            while(1):
+                self.spoon.update()
+                self.spoon.alter_pwm()
 
-            self.leg.update()
-            self.leg.alter_pwm()
+                self.leg.update()
+                self.leg.alter_pwm()
+        except KeyboardInterrupt:
+            print "Keyboard interrupt in SP"
