f6ddd9e9bdabc8a6083f0157f7e673ffaf5e9da6
Martin Lensment <mlensment@gmail.com>
2015-05-11 21:19:09 +0300

    Add timer for eye

--- lib/eye.py
+++ lib/eye.py
@@ -1,9 +1,13 @@
 import math
+import time
 
 class Eye:
     target = None
     current_target_gaze = False
     target_gaze_cache = []
+
+    target_timer_start = Eye.time_in_millis()
+    target_timer = 0
 
     def __init__(self, x = None, y = None):
         self.x = x
@@ -18,10 +22,12 @@ class Eye:
 
         if math.fabs(self.x - Eye.target[0]) <= error and math.fabs(self.y - Eye.target[1]) <= error:
             Eye.current_target_gaze = True
-            self.update_target_gaze_cache(True)
+            # self.update_target_gaze_cache(True)
+            self.update_timer()
         else:
             Eye.current_target_gaze = False
-            self.update_target_gaze_cache(False)
+            # self.update_target_gaze_cache(False)
+            self.reset_timer()
 
         # print str(math.fabs(self.x - Eye.target[0])) + ' ' + str(math.fabs(self.y - Eye.target[1]))
 
@@ -35,6 +41,14 @@ class Eye:
 
     def not_looking_at_target(self):
         return not self.is_looking_at_target()
+
+    def reset_timer(self):
+        print Eye.target_timer
+        Eye.target_timer_start = Eye.time_in_millis()
+        Eye.target_timer = 0
+
+    def update_timer(self):
+        Eye.target_timer += Eye.time_in_millis() - Eye.target_timer_start
 
     def update_target_gaze_cache(self, b):
         if len(Eye.target_gaze_cache) < 40:
@@ -53,3 +67,7 @@ class Eye:
             return True
 
         return False
+
+    @staticmethod
+    def time_in_millis():
+        return int(round(time.time() * 1000))
