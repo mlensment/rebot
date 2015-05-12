93cde767ebaebf9401e639a112cd9cace7a234eb
Martin Lensment <mlensment@gmail.com>
2015-05-12 15:20:06 +0300

    Add license

--- commit.sh
+++ commit.sh
@@ -7,4 +7,4 @@ else
   git commit -m "$1"
 fi
 
-git push origin master && ssh pi@192.168.0.19 'cd ~/projects/rebot && git pull origin master'
+git push origin master && git push github master && ssh pi@192.168.0.19 'cd ~/projects/rebot && git pull origin master'
